867ab515c9cef4998339fac44680dd0b49bca694
Derick Vigne <derickvigne@me.com>
2016-12-13 01:26:31 -0500

    Initial Commit

--- headers/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo("AY:");
-include_once("../headers/security.php");
-echo("INSERT INTO ADMIN (HASH,USERNAME) VALUES('".Security::makeSaltedHash(Security::DEFAULT_PASSWORD) . "','admin');")
-?>
